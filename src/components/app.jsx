28a467b47f50668ac3dd626d75d34b1a4421ea65
AnN <akimova_anna_25@mail.ru>
2021-10-31 21:25:17 +0300

    Установит Material UI, кастомизирует цветовую тему.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import {lime, lightGreen} from '@mui/material/colors';
 
 import {MainPage} from "./main-page";
 
 
+const THEME = createTheme({
+  palette: {
+    primary: {
+      light: lime[100],
+      main: lime[300],
+      dark: lime[600],
+      contrastText: lightGreen[800],
+    },
+    /* secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    }, */
+  },
+});
+
+
 export const App = () => {
   return (
-    <MainPage />
+    <ThemeProvider theme={THEME}>
+      <MainPage />
+    </ThemeProvider>
   );
 };
--- src/components/main-page.jsx
+++ src/components/main-page.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import {Button} from "@mui/material";
 
 
 export const MainPage = () => {
   return (
     <main>
+      <Button variant="contained">show books list</Button>
     </main>
   );
 };
