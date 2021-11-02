7d59ac840e2f7eea205cf11291e4ebe55163d69b
AnN <akimova_anna_25@mail.ru>
2021-11-02 11:41:50 +0300

    Добавит модуль работы с Api.

---
+++ src/api.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+
+export const createAPI = (onFailRequest) => {
+  const api = axios.create({
+    baseURL: location.origin,
+    timeout: 1000 * 5,
+    withCredentials: true,
+  });
+
+  const onSuccess = (response) => {
+    return response;
+  };
+
+  const onFail = (error) => {
+    onFailRequest(error);
+
+    throw error;
+  };
+
+  api.interceptors.response.use(onSuccess, onFail);
+
+  return api;
+};
