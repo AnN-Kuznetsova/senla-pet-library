ddd2226bb0a43ae4ae8e2cd50153eb2224c674cb
AnN <akimova_anna_25@mail.ru>
2021-11-02 11:59:48 +0300

    Создаст экземпляр api в index.js.

--- src/index.js
+++ src/index.js
@@ -3,17 +3,28 @@ import ReactDom from "react-dom";
 import {Provider} from "react-redux";
 
 import {App} from "./components/app";
+import {createAPI} from "./api";
 import {loadBooks} from "./store/books/books";
 import {loadReaders} from "./store/readers/readers";
 import {store} from "./store/store";
 
 
+const onFailRequest = (error) => {
+  // обработать ошибку загрузки данных
+};
+
+const api = createAPI(onFailRequest);
+
+const renderDom = () => {
+  ReactDom.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.querySelector(`#root`)
+  );
+};
+
 const books = store.dispatch(loadBooks(require(`./mocks/books.json`)));
 const readers = store.dispatch(loadReaders(require(`./mocks/readers.json`)));
 
-ReactDom.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.querySelector(`#root`)
-);
+renderDom();
