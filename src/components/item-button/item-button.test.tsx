ec1e9179f38e17d5657c6f423a3f98715e2f9409
AnN <akimova_anna_25@mail.ru>
2022-01-12 14:37:32 +0300

    Установит и настроит Enzyme. Добавит тесты для ItemButton.

--- src/components/item-button/item-button.test.tsx
+++ src/components/item-button/item-button.test.tsx
@@ -1,11 +1,37 @@
 import * as React from "react";
+import {shallow} from "enzyme";
 
-import {ItemButton} from "./item-button";
+import {ItemButton, ItemButtonMode} from "./item-button";
 
 
-describe(``, () => {
-  it(``, () => {
-    expect(true).toBeTruthy();
-    //expect(true).toBeFalsy();
+describe(`Render <ItemButton>`, () => {
+  it(`Should render button with correct text and class`, () => {
+    const itemButtonElement = shallow(
+      <ItemButton
+        textValue={`Item-Button`}
+        className={ItemButtonMode.WARNING}
+        onClick={()=>{/**/}}
+      />);
+
+    expect(itemButtonElement.text()).toBe(`Item-Button`);
+    expect(itemButtonElement.hasClass(ItemButtonMode.WARNING));
+  });
+
+  it(`Should ItemButton be pressed`, () => {
+    const onClick = jest.fn();
+
+    const itemButtonElement = shallow(
+      <ItemButton
+        onClick={onClick}
+      />);
+
+    expect(onClick).toBeCalledTimes(0);
+
+    itemButtonElement.simulate(`click`);
+    expect(onClick).toBeCalledTimes(1);
+
+    itemButtonElement.simulate(`click`);
+    itemButtonElement.simulate(`click`);
+    expect(onClick).toBeCalledTimes(3);
   });
 });
