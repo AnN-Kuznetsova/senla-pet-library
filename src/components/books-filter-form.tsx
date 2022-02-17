d2c6a8cc725a4aa74a63cb597a9d45f7986120fe
AnN <akimova_anna_25@mail.ru>
2022-02-17 18:23:22 +0300

    Вернет BooksFilterForm к прежней реализации через useState.

--- src/components/books-filter-form.tsx
+++ src/components/books-filter-form.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import {FormControl, Input, InputLabel} from "@mui/material";
 import {useEffect, useCallback, useMemo, useState} from "react";
-import {useFormik} from "formik";
 
 import {debounced} from "../utils";
 import type {BookFilterType} from "../types";
@@ -14,21 +13,23 @@ interface PropsType {
 
 const BooksFilterForm: React.FC<PropsType> = (props: PropsType): JSX.Element => {
   const {setBooksFilter} = props;
+  const [title, setTitle] = useState("");
+  const [autor, setAutor] = useState("");
 
-  const formik = useFormik({
-    initialValues: {
-      filterTitle: ``,
-      filterAutor: ``,
-    },
-    onSubmit: () => {/**/},
-  });
+  const handleInputTitleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const handleInputAutorSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAutor(event.target.value);
+  };
 
   const setFilter = useCallback(() => {
     setBooksFilter({
-      title: formik.values.filterTitle,
-      autor: formik.values.filterAutor,
+      title,
+      autor,
     });
-  }, [formik.values.filterTitle, formik.values.filterAutor, setBooksFilter]);
+  }, [title, autor, setBooksFilter]);
 
   const debouncedSetFilter = useMemo(
     () => debounced(setFilter),
@@ -38,17 +39,17 @@ const BooksFilterForm: React.FC<PropsType> = (props: PropsType): JSX.Element => 
   useEffect(() => {
     debouncedSetFilter();
     return debouncedSetFilter.cancel;
-  }, [formik.values.filterTitle, formik.values.filterAutor, debouncedSetFilter]);
+  }, [title, autor, debouncedSetFilter]);
 
   return (
     <form className="form" >
       <FormControl className="form__field-control">
-        <InputLabel htmlFor="filterTitle">Введите название книги</InputLabel>
-        <Input id="filterTitle" name="filterTitle" type="text" value={formik.values.filterTitle} onChange={formik.handleChange} />
+        <InputLabel htmlFor="book-title-filter">Введите название книги</InputLabel>
+        <Input id="book-title-filter" name="book-title-filter" type="text" value={title} onChange={handleInputTitleSearchChange} />
       </FormControl>
       <FormControl className="form__field-control">
-        <InputLabel htmlFor="filterAutor">Введите автора</InputLabel>
-        <Input id="filterAutor" name="filterAutor" type="text" value={formik.values.filterAutor} onChange={formik.handleChange} />
+        <InputLabel htmlFor="book-autor-filter">Введите автора</InputLabel>
+        <Input id="book-autor-filter" name="book-autor-filter" type="text" value={autor} onChange={handleInputAutorSearchChange} />
       </FormControl>
     </form>
   );
